5f2aaacc578a08afcb5d4bf1683634d9e5228b7c
Michael Orlando <mikeaorlando@gmail.com>
2019-09-27 21:04:20 -0500

    added many cats 🐱

--- data_download.R
+++ data_download.R
@@ -1,22 +1,60 @@
-#download prcl.mdb and load tables into lists prefixed with prcl.
+#
+cat("Loading MDB files from stlouis-mo.gov/data/ into R global namespace.")
+#
+
+#
+cat("download prcl.mdb and load tables into lists prefixed with prcl.")
+#
 download.file("https://www.stlouis-mo.gov/data/upload/data-files/prcl.zip", "prcl.zip")
+cat("prcl.zip is downloaded.\n")
+
 unzip("prcl.zip")
+cat("prcl.zip is unzipped\n")
+
 prcl_db <- mdb.get('prcl.mdb')
+cat("prcl_db is loaded from prcl.mdb.\n")
+
+cat("Loading tables in one thread:\n")
 list2env(setNames(prcl_db, paste0("prcl.", names(prcl_db))), .GlobalEnv)
+cat("Loaded all prcl.* tables into the R global namespace process.\n")
+
 remove(prcl_db)
+cat("Removed prcl_db variable from R global namespace.\n")
+
 file.remove(c("prcl.mdb","prcl.zip"))
+cat("Removed prcl.mdb and prcl.zip files removed from computer.\n")
 
-#download bldginsp.mdb and load tables into lists prefixed with bldginsp.
+#
+cat("download bldginsp.mdb and load tables into lists prefixed with bldginsp.")
+#
 download.file("https://www.stlouis-mo.gov/data/upload/data-files/bldginsp.zip", "bldginsp.zip")
-unzip("bldginsp.zip")
-bldginsp_db <- mdb.get('bldginsp.mdb')
-list2env(setNames(bldginsp_db, paste0("bldginsp.", names(bldginsp_db))), .GlobalEnv)
-remove(bldginsp_db)
-file.remove(c("bldginsp.mdb","bldginsp.zip"))
+cat("bldginsp.zip is downloaded.\n")
 
+unzip("bldginsp.zip")
+cat("bldginsp.zip is unzipped\n")
+
+bldginsp_db <- mdb.get('bldginsp.mdb')
+cat("bldginsp_db is loaded from bldginsp.mdb.\n")
+
+cat("Loading tables in one thread:\n")
+list2env(setNames(bldginsp_db, paste0("bldginsp.", names(bldginsp_db))), .GlobalEnv)
+cat("Loaded all bldginsp.* tables into the R global namespace process.\n")
+
+remove(bldginsp_db)
+cat("Removed bldginsp_db variable from R global namespace.\n")
+
+file.remove(c("bldginsp.mdb","bldginsp.zip"))
+cat("Removed pbldginsp.mdb and bldginsp.zip files removed from computer.\n")
+
+#
 #download prmbdo.mdb and load tables into lists prefixed with prmbdo.
+#
 download.file("https://www.stlouis-mo.gov/data/upload/data-files/prmbdo.zip", "prmbdo.zip")
+cat("prmbdo.zip is downloaded.\n")
+
 unzip("prmbdo.zip")
+cat("prmbdo.zip is downloaded.\n")
+
 prmbdo_db <- mdb.get('prmbdo.mdb')
 prmcode_db <- mdb.get('prmcode.mdb')
 list2env(setNames(prmbdo_db, paste0("prmbdo.", names(prmbdo_db))), .GlobalEnv)
@@ -25,7 +63,9 @@ remove(prmbdo_db)
 remove(prmcode_db)
 file.remove(c("prmbdo.mdb","prmbdo.zip", 'prmcode.mdb'))
 
+#
 #download prmemp.mdb and load tables into lists prefixed with prmemp.
+#
 download.file("https://www.stlouis-mo.gov/data/upload/data-files/prmemp.zip", "prmemp.zip")
 unzip("prmemp.zip")
 prmemp_db <- mdb.get('prmemp.mdb')
@@ -35,3 +75,7 @@ list2env(setNames(prmcode2_db, paste0("prmcode2.", names(prmcode2_db))), .Global
 remove(prmemp_db)
 remove(prmcode2_db)
 file.remove(c("prmemp.mdb","prmemp.zip", 'prmcode.mdb'))
+
+#
+cat("data_download.R is finished.")
+#
